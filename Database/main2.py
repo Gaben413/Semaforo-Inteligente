842497b54c1c2a0e22f24cf6566b5cf05d7063a1
NowhereCat <gabrielbportela413@gmail.com>
2023-04-28 13:38:06 -0300

    renamed files

--- Database/main2.py
+++
@@ -1,9 +0,0 @@
-from main import FetchData, INSERT_DATA
-from datetime import datetime
-
-data = {"counter": 77, "date_time": datetime.now()}
-
-INSERT_DATA(1, data)
-
-data = {"counter": 7, "date_time": datetime.now()}
-#print(FetchData(datetime.fromisoformat('2023-04-28 07:00:00'), (60*30), 3))
